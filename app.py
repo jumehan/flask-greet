c1a3446ff617db3cd29749503033306074fc72ab
Julie Han <juliemenghan@gmail.com>
2022-07-27 15:20:58 -0400

    greet done

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.get("/welcome")
+def welcome():
+    '''Returns a welcome message'''
+
+    html = "<html><p>welcome</p></html>"
+    return html
+
+@app.get("/welcome/home")
+def welcome_home():
+    '''Returns a welcome home message'''
+
+    html = "<html><p>welcome home</p></html>"
+    return html
+
+@app.get("/welcome/back")
+def welcome_back():
+    '''Returns a welcome back message'''
+
+    html = "<html><p>welcome back</p></html>"
+    return html
